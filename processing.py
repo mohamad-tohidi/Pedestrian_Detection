9ff66fba0205512244e16b2095bd5d2c7890941c
Mohammad Tohidi <138975632+mohamad-tohidi@users.noreply.github.com>
2023-10-22 10:54:55 -0700

    Add files via upload

---
+++ processing.py
@@ -0,0 +1,101 @@
+# processing.py
+
+import os
+import numpy as np
+import supervision as sv
+from ultralytics import YOLO
+import cv2
+import pygame
+from shapely.geometry import Point, Polygon
+
+
+pygame.mixer.init()
+warning_sound = pygame.mixer.Sound('warning1.wav')
+
+MODEL_PATH = 'yolov8n_openvino_model/'
+CLASS_ID = 0
+SKIP_RATE = 8
+frame_count = 0
+
+def process_frame(frame, _):
+    global frame_count
+
+    if frame_count % SKIP_RATE == 0:
+        results = model(frame, imgsz=384)[0]
+        detections = sv.Detections.from_yolov8(results)
+        detections = detections[detections.class_id == CLASS_ID]
+
+        # Convert the polygon_coords to a Shapely Polygon
+        polygon_shape = Polygon(zone.polygon)
+
+        # Loop through each detection to check if it's inside the danger zone
+        for bbox in detections.xyxy:
+            x1, y1, x2, y2 = bbox
+            x_center = (x1 + x2) / 2
+            y_center = (y1 + y2) / 2
+            point = Point(x_center, y_center)
+
+            if polygon_shape.contains(point):
+                warning_sound.play()  # Play the preloaded warning sound instantly
+                break
+
+        zone.trigger(detections=detections)
+        frame = box_annotator.annotate(scene=frame, detections=detections)
+
+    frame = zone_annotator.annotate(scene=frame)
+
+    frame_count += 1
+    return frame
+
+def process_video_with_annotations(video_path, polygon_coords):
+    global model, zone, box_annotator, zone_annotator
+
+    model = YOLO(MODEL_PATH)
+    video_info = sv.VideoInfo.from_video_path(video_path)
+
+    polygon = np.array(polygon_coords)
+    zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=video_info.resolution_wh)
+    box_annotator = sv.BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
+    zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white(), thickness=6, text_thickness=6, text_scale=4)
+
+    # Get the output video path
+    output_video_path = video_path.rsplit('.', 1)[0] + "-results.mp4"
+    sv.process_video(source_path=video_path, target_path=output_video_path, callback=process_frame)
+
+def process_webcam_with_annotations(polygon_coords):
+    global model, zone, box_annotator, zone_annotator, frame_count
+
+    # Initialize the YOLO model
+    model = YOLO(MODEL_PATH)
+
+    # Set webcam resolution
+    cap = cv2.VideoCapture(0)
+
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame from webcam.")
+        return
+    frame_resolution_wh = (frame.shape[1], frame.shape[0])
+
+    polygon = np.array(polygon_coords)
+    zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=frame_resolution_wh)
+    box_annotator = sv.BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
+    zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white(), thickness=6, text_thickness=6, text_scale=4)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Process the frame
+        processed_frame = process_frame(frame, None)
+
+        # Show the processed frame
+        cv2.imshow("Processed Webcam Feed", processed_frame)
+
+        # Break the loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ ui.py
@@ -0,0 +1,128 @@
+# ui.py
+
+import tkinter as tk
+from tkinter import filedialog
+import cv2
+from PIL import Image, ImageTk
+import processing
+
+polygon_points = []
+video_path = ""
+
+MAX_WIDTH = 640
+MAX_HEIGHT = 480
+
+def resize_image(image, max_width=MAX_WIDTH, max_height=MAX_HEIGHT):
+    global resize_ratio  # Declare resize_ratio as global to access it later
+
+    width_ratio = max_width / image.width
+    height_ratio = max_height / image.height
+    resize_ratio = min(width_ratio, height_ratio)
+
+    new_width = int(image.width * resize_ratio)
+    new_height = int(image.height * resize_ratio)
+
+    return image.resize((new_width, new_height), Image.LANCZOS)
+
+is_webcam_mode = False  # Flag to check if webcam mode is active
+
+def load_video():
+    global video_path, is_webcam_mode, cap
+    if is_webcam_mode:
+        cap = cv2.VideoCapture(0)  # Open the default camera (webcam)
+        update_canvas_with_webcam_feed()
+        if ret:
+            img = Image.fromarray(cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB))
+            img = resize_image(img)
+            imgtk = ImageTk.PhotoImage(image=img)
+            video_canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
+            video_canvas.image = imgtk
+    else:
+        video_path = filedialog.askopenfilename(title="Select a Video")
+        video_input_label.config(text=video_path)
+
+        cap = cv2.VideoCapture(video_path)
+        ret, first_frame = cap.read()
+        cap.release()
+        if ret:
+            img = Image.fromarray(cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB))
+            img = resize_image(img)
+            imgtk = ImageTk.PhotoImage(image=img)
+            video_canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
+            video_canvas.image = imgtk
+
+def toggle_input_mode():
+    global is_webcam_mode
+    is_webcam_mode = not is_webcam_mode
+    if is_webcam_mode:
+        video_input_button.config(text="Load Webcam Feed")
+        video_input_label.config(text="Webcam Mode Active")
+    else:
+        video_input_button.config(text="Load Video")
+        video_input_label.config(text="No video selected.")
+
+def update_canvas_with_webcam_feed():
+    ret, frame = cap.read()
+    if ret:
+        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        img = resize_image(img)
+        imgtk = ImageTk.PhotoImage(image=img)
+        video_canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
+        video_canvas.image = imgtk
+        root.after(10, update_canvas_with_webcam_feed)  # Refresh every 10ms
+    else:
+        print("Failed to grab frame from webcam.")
+
+
+def on_canvas_click(event):
+    global polygon_points
+    x, y = event.x, event.y
+    video_canvas.create_oval(x-2, y-2, x+2, y+2, fill="red", width=2)
+    polygon_points.append([x, y])
+
+def rescale_coordinates(coords):
+    """Rescale coordinates to original size."""
+    width_ratio = MAX_WIDTH / video_canvas.winfo_width()
+    height_ratio = MAX_HEIGHT / video_canvas.winfo_height()
+
+    # Assuming uniform scaling for both width and height
+    scaling_factor = min(width_ratio, height_ratio)
+
+    return [[int(x / scaling_factor) for x in point] for point in coords]
+
+
+def start_processing():
+    global video_path, polygon_points, is_webcam_mode, cap
+    if is_webcam_mode:
+        cap.release()  # Release the webcam
+        rescaled_polygon = rescale_coordinates(polygon_points)
+        root.destroy()
+        processing.process_webcam_with_annotations(rescaled_polygon)
+    elif video_path and polygon_points:
+        rescaled_polygon = rescale_coordinates(polygon_points, resize_ratio)
+        root.destroy()
+        processing.process_video_with_annotations(video_path, rescaled_polygon)
+
+
+root = tk.Tk()
+root.title("Video Annotator")
+
+control_frame = tk.Frame(root)
+control_frame.pack(pady=20)
+
+toggle_mode_button = tk.Button(control_frame, text="Toggle Webcam/Video Mode", command=toggle_input_mode)
+toggle_mode_button.grid(row=0, column=2, padx=10)
+
+video_input_label = tk.Label(control_frame, text="No video selected.")
+video_input_label.grid(row=0, column=0, padx=10)
+video_input_button = tk.Button(control_frame, text="Load Video", command=load_video)
+video_input_button.grid(row=0, column=1, padx=10)
+
+video_canvas = tk.Canvas(root, bg="white", width=MAX_WIDTH, height=MAX_HEIGHT)
+video_canvas.pack(pady=20)
+video_canvas.bind("<Button-1>", on_canvas_click)
+
+start_button = tk.Button(root, text="Start Processing", command=start_processing)
+start_button.pack(pady=20)
+
+root.mainloop()
